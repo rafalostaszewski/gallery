3e68a8c7bc1c1ba343a515ee24f875634cd20f8d
Rafal Ostaszewski <ostaszewski90@gmail.com>
2022-04-19 15:30:30 +0200

    exracting UNSPLASH_KEY to the .env.local file

--- api/app.py
+++ api/app.py
@@ -1,9 +1,15 @@
-from urllib import response
+import os
 import requests
 from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL='https://api.unsplash.com/photos/random'
-UNSPLASH_KEY='Km63AEhhfAMPS4gtH5SMiSX9Gshl5AxOKmooHWy0NIw'
+UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Please create .env.local file and inset there UNSPLASH_KEY")
 
 app = Flask(__name__)
 
