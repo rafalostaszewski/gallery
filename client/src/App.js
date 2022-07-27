c48f0c597b82c686f3ec047efdcd7caec1922226
Rafal <ostaszewski90@gmail.com>
2022-07-27 16:53:58 -0300

    toast notification for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
@@ -22,6 +24,7 @@ const App = () => {
       setLoading(false);
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
 
@@ -33,8 +36,10 @@ const App = () => {
     try {
       const res = await axios.get(`${API_URL}/new-image?query=${word}`);
       setImages([{ ...res.data, tittle: word }, ...images]);
+      toast.info(`New image ${word.toUpperCase()} was found`);
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
 
     setWord('');
@@ -44,10 +49,16 @@ const App = () => {
     try {
       const res = await axios.delete(`${API_URL}/images/${id}`);
       if (res.data?.deleted_id) {
+        toast.warn(
+          `Image ${images
+            .find((i) => i.id === id)
+            .tittle.toUpperCase()} was deleted`
+        );
         setImages(images.filter((image) => image.id !== id));
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
 
@@ -61,11 +72,15 @@ const App = () => {
         setImages(
           images.map((image) =>
             image.id === id ? { ...image, saved: true } : image
+          ),
+          toast.success(
+            `Image ${imageToBeSaved.tittle.toUpperCase()} was saved`
           )
         );
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
 
@@ -100,6 +115,7 @@ const App = () => {
           </Container>
         </>
       )}
+      <ToastContainer position="bottom-right" />
     </div>
   );
 };
